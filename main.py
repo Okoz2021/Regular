3270d078a6d8b7799d5dada26783867a00588341
Olga Kozionova <okoz2013@gmail.com>
2021-12-03 20:53:57 +0500

    var2

--- main.py
+++ main.py
@@ -1,49 +1,56 @@
 import re
+from pprint import pprint
 import csv
-with open("phonebook_raw.csv", encoding = 'utf-8') as f:
-  rows = csv.reader(f, delimiter=",")
-  contacts_list = list(rows)
+
+PHONE_PATTERN = r'(\+7|8)*[\s\(]*(\d{3})[\)\s-]*(\d{3})[-]*(\d{2})[-]*(\d{2})[\s\(]*(доб\.)*[\s]*(\d+)*[\)]*'
+PHONE_SUB = r'+7(\2)-\3-\4-\5 \6\7'
+
+with open("phonebook_raw.csv", encoding="utf-8") as f:
+    rows = csv.reader(f, delimiter=",")
+    contacts_list = list(rows)
 
 
-pattern1 = r'^([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]+)(\s*)(\,?)([А-ЯЁа-яё]*)(\,?)(\,?)(\,?)'
-new_pattern1 = r'\1\3\10\4\6\9\7\8'
-new_contact_list1 = list()
-for name in contacts_list:
-  as_string1 = ','.join(name)
-  formatted_card1 = re.sub(pattern1, new_pattern1, as_string1)
-  as_list1 = formatted_card1.split(',')
-  new_contact_list1.append(as_list1)
+# TODO 1: выполните пункты 1-3 ДЗ
 
-pattern2 = r'(\+7|8)(\s*)(\(*)(\d{3})(\)*)(\s*)(\-*)(\d{3})(\s*)(\-*)(\d{2})(\s*)(\-*)(\d{2})(\s*)(\(*)(доб)*(\.*)(\s*)(\d+)*(\)*)'
-new_pattern2 = r'+7(\4)\8-\11-\14\15\17\18\19\20'
-new_contact_list2= list()
-for number in new_contact_list1:
-  as_string2 = ','.join(number)
-  formatted_card = re.sub(pattern2, new_pattern2, as_string2)
-  card_as_list = formatted_card.split(',')
-  new_contact_list2.append(card_as_list)
-
-for i in new_contact_list2:
-  for j in new_contact_list2:
-    if i[0] == j[0] and i[1] == j[1] and i is not j:
-      if i[2] == '':
-        i[2] = j[2]
-      if i[3] == '':
-        i[3] = j[3]
-      if i[4] == '':
-         i[4] = j[4]
-      if i[5] == '':
-         i[5] = j[5]
-      if i[6] == '':
-         i[6] = j[6]
-contacts_list_updated = list()
-for duplicates in new_contact_list2:
-  if duplicates not in contacts_list_updated:
-    contacts_list_updated.append(duplicates)
+def main(contact_list: list):
+    """основная логика"""
+    new_list = list()
+    for item in contact_list:
+        full_name = ' '.join(item[:3]).split(' ')
+        result = [full_name[0], full_name[1], full_name[2], item[3], item[4],
+                  re.sub(PHONE_PATTERN, PHONE_SUB, item[5]),
+                  item[6]]
+        new_list.append(result)
+    return union(new_list)
 
 
+def union(contacts: list):
+    """функция обработки списка от одинаковых и пустых записях"""
+    for contact in contacts:
+        first_name = contact[0]
+        last_name = contact[1]
+        for new_contact in contacts:
+            new_first_name = new_contact[0]
+            new_last_name = new_contact[1]
+            if first_name == new_first_name and last_name == new_last_name:
+                if contact[2] == "": contact[2] = new_contact[2]
+                if contact[3] == "": contact[3] = new_contact[3]
+                if contact[4] == "": contact[4] = new_contact[4]
+                if contact[5] == "": contact[5] = new_contact[5]
+                if contact[6] == "": contact[6] = new_contact[6]
 
-print(contacts_list_updated)
-with open("phonebook_new.csv", "w", encoding = 'utf-8') as f:
-  datawriter = csv.writer(f, delimiter=',')
-  datawriter.writerows(contacts_list_updated)
+    result_list = list()
+    for i in contacts:
+        if i not in result_list:
+            result_list.append(i)
+
+    return result_list
+
+
+# TODO 2: сохраните получившиеся данные в другой файл
+# код для записи файла в формате CSV
+with open("phonebook_true.csv", "w", encoding="utf-8") as f:
+    datawriter = csv.writer(f, delimiter=',')
+    datawriter.writerows(main(contacts_list))
+
+
